568a803a264fdf91ab0eb9d3fc755a5c7793109e
Eduardo Pulido <pul21010@byui.edu>
2025-03-05 20:06:44 -0600

    CSE341 First Assignment

--- controllers/week01.js
+++ controllers/week01.js
@@ -13,6 +13,10 @@ const route03 = async (req, res) => {
   try {
     const db = getDb().db('mdb_professional'); 
     const users = await db.collection('professionalList').find().toArray();
+    if (!users || users.length === 0) {
+      return res.status(404).json({ error: "No professionals found" });
+    }
+    res.setHeader("Content-Type", "application/json");
     res.json(users);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch users' });
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const { initDb} = require('./models/connect');
 
 app.use('/', require('./routes'));
 app.use(express.json());
+app.use(cors());
 
 // Serve the frontend folder
 app.use(express.static(path.join(__dirname, './cse341-ww-student-code/frontend')));
